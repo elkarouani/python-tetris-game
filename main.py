b91707ca3ac5b1c4171bdb8fd731a1b0c546b83f
KDragon <elkarouani@gmail.com>
2019-01-04 22:09:10 +0100

    Task: adding creating grid function

--- main.py
+++ main.py
@@ -155,6 +155,13 @@ class Piece(object):
 
 def create_grid(locked_positions={}):
     grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_positions:
+                c = locked_positions[(j, i)]
+                grid[i][j] = c
+    return grid
  
 def convert_shape_format(shape):
     pass
